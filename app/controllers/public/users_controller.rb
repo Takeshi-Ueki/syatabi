517636db5d0c0b2a424e617103516ea97dab0d11
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-07-04 08:52:12 +0000

    sidebar public/user Finish

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,11 +1,16 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def check
@@ -13,4 +18,9 @@ class Public::UsersController < ApplicationController
 
   def withdraw
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :profile, :profile_image)
+    end
 end
