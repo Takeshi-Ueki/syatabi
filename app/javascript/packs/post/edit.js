e03ba25998597587c521c2a278afb101529f1a52
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-07-26 06:52:42 +0000

    post edit js編集

--- app/javascript/packs/post/edit.js
+++ app/javascript/packs/post/edit.js
@@ -1,12 +1,10 @@
 let map
 let geocoder
-let marker = null
+let marker
 
 // mapの初期設定
 function initMap(){
   geocoder = new google.maps.Geocoder()
-  // const postLat = document.getElementById('lat').value
-  // const postLong = document.getElementById('long').value
 
   map = new google.maps.Map(document.getElementById('map'), {
     //初期位置 latが緯度、lngが経度を示す
@@ -23,28 +21,35 @@ function initMap(){
 
   // positionに指定した座標にピンを表示させる
   marker = new google.maps.Marker({
-    // position: {lat: 35.689009, lng: 139.700707},
     position: {lat: gon.lat, lng: gon.long},
     map: map
+  });
+
+  document.getElementById('search-btn').addEventListener('click', function() {
+    deleteMakers();
+    codeAddress(geocoder, map);
   });
 }
 
 
 // 検索フォームのボタンが押されたときに実行
-function codeAddress(){
+function codeAddress(geocoder, resultMap){
   // 検索フォームの入力内容を取得 id='address'
-const inputAddress = document.getElementById('address').value;
+  let inputAddress = document.getElementById('address').value;
 
   geocoder.geocode({
     'address': inputAddress
   }, function(results, status) {
     // 該当する検索結果がヒットした時に、地図の中身を検索結果の緯度経度に更新
     if (status == 'OK') {
-      map.setCenter(results[0].geometry.location);
+      let latlng = results[0].geometry.location
+      map.setCenter(latlng);
+      post_lat.value = latlng.lat();
+      post_long.value = latlng.lng();
 
-          marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
+      marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
       });
     } else {
       alert('該当する結果がありませんでした：' + status);
