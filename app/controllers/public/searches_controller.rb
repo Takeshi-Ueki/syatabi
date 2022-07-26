5035c001bc4ecea2f733ff0122b4fd091307a038
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-07-26 07:10:18 +0000

    search エラー

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -3,12 +3,12 @@ class Public::SearchesController < ApplicationController
     @word = params[:word]
     @range = params[:range]
     if @range == "ユーザー名"
-      @search_result = User.where("name LIKE?", "%#{@word}%")
+      @search_result = User.where("name LIKE?", "%#{@word}%").page(params[:page]).per(10)
     elsif @range == "プロフィール"
-      @search_result = User.where("profile LIKE?", "%#{@word}%")
+      @search_result = User.where("profile LIKE?", "%#{@word}%").page(params[:page]).per(10)
     else
       @range = "投稿"
-      @search_result = Post.where("body LIKE?","%#{@word}%")
+      @search_result = Post.where("body LIKE?","%#{@word}%").page(params[:page]).per(10)
     end
   end
 end
