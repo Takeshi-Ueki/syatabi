79e10c8270d1f126aa138fb703b63202a47c9a54
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-08-16 12:33:13 +0000

    route Finish

--- app/javascript/packs/post/show.js
+++ app/javascript/packs/post/show.js
@@ -1,9 +1,10 @@
 let map
+let geocoder
+let request
 
 // mapの初期設定
 function initMap(){
-  // const postLat = document.getElementById('lat').value
-  // const postLong = document.getElementById('long').value
+  geocoder = new google.maps.Geocoder()
 
   map = new google.maps.Map(document.getElementById('map'), {
     //初期位置 latが緯度、lngが経度を示す
@@ -18,6 +19,62 @@ function initMap(){
     position: {lat: gon.lat, lng: gon.long},
     map: map
   });
+
+  document.getElementById('search-btn').addEventListener('click', function() {
+    codeAddress(geocoder, map);
+  });
 }
+
+function codeAddress(geocoder, resultMap){
+  // 検索フォームの入力内容を取得 id='address'
+  let inputAddress = document.getElementById('address').value;
+
+  geocoder.geocode({
+    'address': inputAddress
+  }, function(results, status) {
+    // 該当する検索結果がヒットした時に、地図の中身を検索結果の緯度経度に更新
+    if (status == 'OK') {
+      let latlng = results[0].geometry.location
+      map.setCenter(latlng);
+      search_lat = latlng.lat();
+      search_lng = latlng.lng();
+
+      let directionsService = new google.maps.DirectionsService();
+      let directionsRenderer = new google.maps.DirectionsRenderer();
+      let routeTime = document.getElementById('route-time')
+      let routeDistance = document.getElementById('route-distance')
+
+      directionsRenderer.setMap(map);
+
+      let start = new google.maps.LatLng(search_lat, search_lng);
+      let end = new google.maps.LatLng(gon.lat, gon.long);
+
+      request = {
+        origin: start,
+        destination: end,
+        travelMode: 'DRIVING'
+      };
+
+      directionsService.route(request, function(result, status) {
+        //ステータスがOKの場合、
+        if (status === 'OK') {
+          directionsRenderer.setDirections(result); //取得したルート（結果：result）をセット
+          let duration_text = result.routes[0].legs[0].duration.text;
+          let distance_text = result.routes[0].legs[0].distance.text;
+
+          routeTime.textContent = " 約" + duration_text;
+          routeDistance.textContent = distance_text;
+
+        }else{
+          alert("取得できませんでした：" + status);
+        }
+      });
+    } else {
+      alert('該当する結果がありませんでした：' + status);
+    }
+  });
+}
+
+
 
 window.initMap = initMap;
